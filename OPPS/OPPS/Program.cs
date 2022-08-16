f1eaa0797b8c0d630e693e21acf7e6d72a5b81a2
Dũng Phạm <ptdung240102@gmail.com>
2022-08-16 19:55:48 +0700

    OOP

---
+++ OPPS/OPPS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
